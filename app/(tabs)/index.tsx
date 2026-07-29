a6b649e5c4d520821e7efa67b1fd1c87ae6c719c
aaronCustodio11 <aaronmanuelcustodio11@gmail.com>
2026-07-29 20:02:02 +0800

    Initial Habu project setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
@@ -9,6 +10,10 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      <Link href="/modal" style={styles.link}>
+        <Text style={styles.linkText}>Open modal</Text>
+      </Link>
     </View>
   );
 }
@@ -27,5 +32,13 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  link: {
+    marginTop: 20,
+    paddingVertical: 10,
+  },
+  linkText: {
+    fontSize: 16,
+    color: '#2e78b7',
   },
 });
